68462c599788d9b6d41e8b989d8bcc04134d8286
Malleus Tempus <62674521+speedster33@users.noreply.github.com>
2022-01-13 10:41:45 -0500

    imported rerobotics library

--- src/main/java/frc/robot/subsystems/Wheel.java
+++ src/main/java/frc/robot/subsystems/Wheel.java
@@ -1,8 +1,8 @@
 package frc.robot.subsystems;
 
-// import com.revrobotics.ColorSensorV3;
-// import com.revrobotics.ColorMatchResult;
-// import com.revrobotics.ColorMatch;
+import com.revrobotics.ColorSensorV3;
+import com.revrobotics.ColorMatchResult;
+import com.revrobotics.ColorMatch;
 
 import edu.wpi.first.wpilibj.util.Color;
 import edu.wpi.first.wpilibj.DoubleSolenoid;
@@ -16,9 +16,9 @@ public class Wheel extends SubsystemBase {
     private final Spark m_wheel = new Spark(10);
     private final DoubleSolenoid m_wheelLift = new DoubleSolenoid(1, null, 6, 7);
 
-    // private final ColorSensorV3 m_color = new ColorSensorV3(I2C.Port.kOnboard);
+    private final ColorSensorV3 m_color = new ColorSensorV3(I2C.Port.kOnboard);
 
-    // private final ColorMatch m_colorMatcher = new ColorMatch();
+    private final ColorMatch m_colorMatcher = new ColorMatch();
 
     private static final Color kRed = new Color(0.518311, 0.344971, 0.136963);
     private static final Color kGreen = new Color(0.1689, 0.575439, 0.25585);
@@ -26,10 +26,10 @@ public class Wheel extends SubsystemBase {
     private static final Color kYellow = new Color(0.320068, 0.558105, 0.122070);
 
     public Wheel() {
-        // m_colorMatcher.addColorMatch(kRed);
-        // m_colorMatcher.addColorMatch(kGreen);
-        // m_colorMatcher.addColorMatch(kBlue);
-        // m_colorMatcher.addColorMatch(kYellow);
+        m_colorMatcher.addColorMatch(kRed);
+        m_colorMatcher.addColorMatch(kGreen);
+        m_colorMatcher.addColorMatch(kBlue);
+        m_colorMatcher.addColorMatch(kYellow);
 
         SendableRegistry.setSubsystem(m_wheel, this.getClass().getSimpleName());
         SendableRegistry.setName(m_wheel, "Wheel Spinner Motor");
@@ -40,19 +40,19 @@ public class Wheel extends SubsystemBase {
 
     @Override
     public void periodic() {
-        // ColorMatchResult detectedColor = m_colorMatcher.matchClosestColor(m_color.getColor());
+        ColorMatchResult detectedColor = m_colorMatcher.matchClosestColor(m_color.getColor());
 
-    //     if (detectedColor.color == kRed) {
-    //         SmartDashboard.putString("color_detected", "red");
-    //     } else if (detectedColor.color == kGreen) {
-    //         SmartDashboard.putString("color_detected", "green");
-    //     } else if (detectedColor.color == kBlue) {
-    //         SmartDashboard.putString("color_detected", "blue");
-    //     } else if (detectedColor.color == kYellow) {
-    //         SmartDashboard.putString("color_detected", "yellow");
-    //     } else {
-    //         SmartDashboard.putString("color_detected", "None Colors there be");
-    //     }
+        if (detectedColor.color == kRed) {
+            SmartDashboard.putString("color_detected", "red");
+        } else if (detectedColor.color == kGreen) {
+            SmartDashboard.putString("color_detected", "green");
+        } else if (detectedColor.color == kBlue) {
+            SmartDashboard.putString("color_detected", "blue");
+        } else if (detectedColor.color == kYellow) {
+            SmartDashboard.putString("color_detected", "yellow");
+        } else {
+            SmartDashboard.putString("color_detected", "None Colors there be");
+        }
         }
 
     public void raise() {
