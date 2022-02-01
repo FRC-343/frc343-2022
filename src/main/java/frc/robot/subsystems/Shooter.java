63478fc5f046a72d39d8bfb11691c03192eeda42
Metal in Motion <343MetalInMotion@gmail.com>
2022-02-01 18:42:36 -0500

    Update Shooter.java

--- src/main/java/frc/robot/subsystems/Shooter.java
+++ src/main/java/frc/robot/subsystems/Shooter.java
@@ -3,12 +3,18 @@ package frc.robot.subsystems;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import edu.wpi.first.wpilibj.Encoder;
 import edu.wpi.first.wpilibj.motorcontrol.Spark;
+
+import com.revrobotics.CANSparkMax;
+import com.revrobotics.CANSparkMaxLowLevel.MotorType;
+
 import edu.wpi.first.math.controller.PIDController;
 import edu.wpi.first.math.controller.SimpleMotorFeedforward;
 import edu.wpi.first.util.sendable.SendableRegistry;
 
 public class Shooter extends SubsystemBase {
     private static final double kShootGarbage = 150.0; // rev per sec, for irregular values
+
+    private CANSparkMax m_shooterSparkMax = new CANSparkMax(20, MotorType.kBrushless); // assuming id (20), = channel/port
 
     private final Spark m_shooter = new Spark(5);
 
