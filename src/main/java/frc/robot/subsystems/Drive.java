9df2eefc30c94eee38e5610ac41a395c8aef7f3b
Metal in Motion <343MetalInMotion@gmail.com>
2022-02-23 21:12:42 -0500

    dive things

--- src/main/java/frc/robot/subsystems/Drive.java
+++ src/main/java/frc/robot/subsystems/Drive.java
@@ -74,12 +74,12 @@ public class Drive extends SubsystemBase {
 
         m_odometry = new DifferentialDriveOdometry(Rotation2d.fromDegrees(getHeading()));
 
-         m_leftFollower.setInverted(true);
-         m_rightFollower.setInverted(true);
+         // m_leftFollower.setInverted(true);
+          m_rightFollower.setInverted(true);
         // m_leftGroup.setInverted(false);
         // m_rightGroup.setInverted(true);
-        m_leftEncoder.setReverseDirection(false);
-        m_rightEncoder.setReverseDirection(false);
+        // m_leftEncoder.setReverseDirection(true);
+        // m_rightEncoder.setReverseDirection(true);
 
         SendableRegistry.setSubsystem(m_leftMaster, this.getClass().getSimpleName());
         SendableRegistry.setName(m_leftMaster, "Left Master Drive Motor Thingy");
