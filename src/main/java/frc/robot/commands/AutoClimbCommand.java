51ecd8cb165d9d1a737ef3adb84dcaa793a8e5cf
Malleus Tempus <62674521+speedster33@users.noreply.github.com>
2022-03-21 16:18:27 -0400

    Update AutoClimbCommand.java

--- src/main/java/frc/robot/commands/AutoClimbCommand.java
+++ src/main/java/frc/robot/commands/AutoClimbCommand.java
@@ -21,18 +21,19 @@ public class AutoClimbCommand extends SequentialCommandGroup {
         new InstantCommand(climbing::engage, climbing),
         // wait b/c they are slow
         new WaitCommand(1),
-        //raise up arms for 1 second
+        // raise up arms for 1 second
         new ParallelDeadlineGroup(
             new WaitCommand(1),
             new ClimbArmCommand(climbing, -.8)),
-        // bring them back in so climbing arms can extend out again
+        // bring them back in so climbing arms can continue extending out
         new InstantCommand(climbing::disEngage, climbing),
+        new WaitCommand(1),
         // raise up arms all the way to the high bar
         new ClimbArmCommand(climbing, -.8),
         // push to second bar
         new InstantCommand(climbing::engage, climbing),
-        new WaitCommand(1),
-        //pull down arms for a little bit of time to come off mid rung
+        new WaitCommand(1.5),
+        // pull down arms for a little bit of time to come off mid rung
         new ParallelDeadlineGroup(
             new WaitCommand(1.3),
             new ClimbArmCommand(climbing, .8)));
